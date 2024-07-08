bcc5d5b43b87be59137fca6824d160f2678e9639
ImKavinduSandaruwan <githubsandaruwan651@gmail.com>
2024-07-09 05:06:40 +0530

    Made image displayed full screen when review and added two more rows to the csv file

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class ImageApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: ChangeNotifierProvider(
         create: (context) => FolderManagerModel(),
-        child: HomePage(),
+        child: Login(),
       ),
     );
   }
